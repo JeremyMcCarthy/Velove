e20cb4683aa1c8cdb3a6116ec7aaa602ef94134d
Victor Plot <victor.plot@etu.emse.fr>
2018-11-30 14:21:43 +0100

    Functionning details activity fix : doesn't erase data from mapsfragment anymore

--- app/src/main/java/com/example/jrmy/velove/DetailsActivity.java
+++ app/src/main/java/com/example/jrmy/velove/DetailsActivity.java
@@ -1,8 +1,13 @@
 package com.example.jrmy.velove;
 
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -15,12 +20,19 @@ public class DetailsActivity extends AppCompatActivity {
         TextView tvStationName = findViewById(R.id.stationName);
         tvStationName.setText(station.getName());
         TextView tvStationAddress = findViewById(R.id.stationAddress);
-        tvStationAddress.setText(station.getAdress()+station.getAdress2());
+        tvStationAddress.setText(station.getAdress()+"\n"+station.getAdress2());
         TextView tvAvailableBikes = findViewById(R.id.availableBikes);
         tvAvailableBikes.setText(""+station.getAvailableBikes());
         TextView tvAvailableBikeStands = findViewById(R.id.availableBikeStands);
         tvAvailableBikeStands.setText(""+station.getAvailableBikeStands());
         TextView tvLastUpdate = findViewById(R.id.lastUpdate);
         tvLastUpdate.setText(station.getLastUpdate());
+
+        //limite de une requête par jour ...
+        ImageView imageView =findViewById(R.id.imageView3);
+        Log.d("STREET","entering");
+        String url = "https://maps.googleapis.com/maps/api/streetview?size=400x400&location="+station.getLatitude()+","+station.getLongitude()+"&fov=90&heading=235&key=AIzaSyDrtYUze0KhgopDcNrjR4OiNTeAm9kAoMo";
+        Picasso.with(getApplicationContext()).load(url).into(imageView);
+        Log.d("STREET","exiting");
     }
 }
